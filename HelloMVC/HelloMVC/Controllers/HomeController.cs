10c22875c0da23bf0abbebe720604a8357e628dc
suban.p@hotmail.co.uk <suban.purushothmakumar@euromoneyplc.com>
2017-01-25 23:45:11 +0000

    Initial commit

---
+++ HelloMVC/HelloMVC/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelloMVC.Models;
+
+namespace HelloMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            if(Session["user"] == null)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+
+            var cars = HelloModel.GetCars();
+            ViewBag.SubanCreatedThis = "Fat Controller's Stag";
+
+            return View(cars);
+        }
+    }
+}
---
+++ HelloMVC/HelloMVC/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelloMVC.Models;
+
+namespace HelloMVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string name, string password)
+        {
+            if("sazfc".Equals(name) && "sazfc".Equals(password))
+            {
+                Session["user"] = new User() { Login = name, Name = "Saz" };
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction( "Index", "Home");
+        }
+    }
+}
